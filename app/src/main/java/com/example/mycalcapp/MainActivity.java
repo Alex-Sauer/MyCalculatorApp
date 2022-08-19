a63f52a2c6667374fb032a6f5b37d8bdb996509a
Alex-Sauer <90796714+Alex-Sauer@users.noreply.github.com>
2022-08-19 08:17:12 -0500

    need to finish calculation in MA activity

--- app/src/main/java/com/example/mycalcapp/MainActivity.java
+++ app/src/main/java/com/example/mycalcapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.mycalcapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, Solve.class);
+        intent.putExtra("NAME", solve());
+        startActivity(intent);
     }
+
+    public Integer solve(int a){
+        if(a==1){
+
+        }else if(a==2){
+
+        }else if(a==3){
+
+        }else if(a==4){
+
+        }
+    }
+
+    public void switchScreens(View view){
+        Intent intent = getIntent();
+        String nameToShow = intent.getStringExtra("");
+        TextView topTxt = findViewById(R.id.topText);
+        TextView botTxt = findViewById(R.id.bottomText);
+        String topNum = topTxt.getText().toString();
+        String botNum = botTxt.getText().toString();
+        int num1= Integer.parseInt(topNum);
+        int num2= Integer.parseInt(botNum);
+    }
+
 }
---
+++ app/src/main/java/com/example/mycalcapp/Solve.java
@@ -0,0 +1,21 @@
+package com.example.mycalcapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Solve extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solve);
+        Intent intent = getIntent();
+        String nameToShow = intent.getStringExtra("SOLVE");
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(nameToShow);
+    }
+}
